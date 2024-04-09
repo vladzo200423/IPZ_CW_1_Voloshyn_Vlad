3914145545c8de01c9b58b5aeb9a9b75b8235d6d
vladzo200423 <82802815+vladzo200423@users.noreply.github.com>
2024-04-09 16:38:26 +0300

    Логіка авторизації

--- app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
@@ -7,16 +7,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import ua.edu.lntu.ipz_cw_1_2.ui.theme.IPZ_CW_1_2_Voloshyn_VladTheme
@@ -41,9 +48,9 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting() {
-    var text by remember { mutableStateOf(TextFieldValue()) }
-    var text1 by remember { mutableStateOf(TextFieldValue()) }
-    var text2 by remember { mutableStateOf(TextFieldValue()) }
+    var text by remember { mutableStateOf("") }
+    var text1 by remember { mutableStateOf("") }
+    var text2 by remember { mutableStateOf("") }
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -63,24 +70,30 @@ fun Greeting() {
                 .padding(paddingValues)
         ) {
             TextField(
-                value = text,
-                onValueChange = {text = it},
-                label = {Text("Введіть email")}
-            )
-            TextField(
                 value = text1,
                 onValueChange = {text1 = it},
                 label = {Text("Введіть email")}
             )
-            Text(text = "Ви ввійшли")
-            Button(onClick = { /*TODO*/ }) {
+            TextField(
+                value = text2,
+                onValueChange = {text2 = it},
+                label = {Text("Введіть email")}
+            )
+            Text(text = text)
+            Button(onClick = {
+                if(text1 != "" && text2 != ""){
+                    text = "Успішна авторизація"
+                }
+                else{
+                    text = "Помилка авторизації"
+                }
+            }) {
                 Text("Sing In")
                 
             }
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
