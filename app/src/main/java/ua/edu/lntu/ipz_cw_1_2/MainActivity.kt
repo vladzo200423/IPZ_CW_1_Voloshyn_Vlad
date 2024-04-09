de1b1f0b7e48ac3aaa769df5e3a33c4e043e5b59
vladzo200423 <82802815+vladzo200423@users.noreply.github.com>
2024-04-09 16:21:55 +0300

    верхня панель

--- app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_1_2/MainActivity.kt
@@ -3,13 +3,22 @@ package ua.edu.lntu.ipz_cw_1_2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.ipz_cw_1_2.ui.theme.IPZ_CW_1_2_Voloshyn_VladTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +31,43 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Sing In",
+                        fontSize = 24.sp
+                    )
+                })
+        }
+    ){
+        paddingValues ->
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IPZ_CW_1_2_Voloshyn_VladTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
